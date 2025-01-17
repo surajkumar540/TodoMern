b8368b34a6b66927d35bfa89ba0901a4ce33b34e
suraj <surajgautam56878@gmail.com>
2025-01-17 18:55:20 +0530

    add:post api created and integrated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,7 @@ import Home from "./Home";
 
 function App() {
   return (
-    <>
       <Home />
-    </>
   );
 }
 
--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 
 const Home = () => {
@@ -10,16 +11,21 @@ const Home = () => {
 
   const handleAdd = () => {
     if (task.trim() === "") return;
-    if (list.includes(task)) return;
-    setList([...list, task]);
+    // if (list.includes(task)) return;
+
+    axios
+      .post("http://localhost:3001/add", { task: task })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+    // // setList([...list, task]);
     setTask("");
   };
 
-  const handleKeyPress = (e: React.KeyboardEvent) => {
-    if (e.key === "Enter") {
-      handleAdd();
-    }
-  };
+  // const handleKeyPress = (e: React.KeyboardEvent) => {
+  //   if (e.key === "Enter") {
+  //     handleAdd();
+  //   }
+  // };
 
   const handleDelete = (index: number) => {
     const filteredList = list.filter((_, keyIndx) => {
@@ -41,7 +47,7 @@ const Home = () => {
             placeholder="Enter your task"
             value={task}
             onChange={handleChange}
-            onKeyPress={handleKeyPress}
+            // onKeyPress={handleKeyPress}
             className="flex-1 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
           />
           <button
